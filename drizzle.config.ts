1c5b8d920df3b958dd84bc60a6d6396080b74d7c
yashsrivastavaaa <yashsrivns@gmail.com>
2025-08-10 19:51:23 +0530

    fix multiple bugs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
     out: './drizzle',
-    schema: './config/recipeHomeSchema.ts',
+    schema: './config/feedbackSchema.ts',
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.NEXT_PUBLIC_DB_API!,
